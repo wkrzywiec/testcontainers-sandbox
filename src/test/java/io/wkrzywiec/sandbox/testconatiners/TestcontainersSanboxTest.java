52e5f54873f70895c46a16150ecda0e37cf84456
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-12-05 16:22:12 +0100

    keycloak example

--- src/test/java/io/wkrzywiec/sandbox/testconatiners/TestcontainersSanboxTest.java
+++ src/test/java/io/wkrzywiec/sandbox/testconatiners/TestcontainersSanboxTest.java
@@ -3,21 +3,17 @@ package io.wkrzywiec.sandbox.testconatiners;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.util.ClassUtils;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.jdbc.ContainerDatabaseDriver;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import javax.sql.DataSource;
-import java.lang.reflect.InvocationTargetException;
-import java.sql.Driver;
 
+import static io.restassured.RestAssured.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
@@ -28,39 +24,47 @@ public class TestcontainersSanboxTest {
     private PostgreSQLContainer posgtres = new PostgreSQLContainer("postgres:9.6.12")
             .withDatabaseName("public");
 
-    // will be started before and stopped after each test method
-//    @Container
-//    private PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer()
-//            .withDatabaseName("foo")
-//            .withUsername("foo")
-//            .withPassword("secret");
+    // will be shared between test methods
+    @Container
+    private static final GenericContainer keycloak = new GenericContainer(DockerImageName.parse("jboss/keycloak:11.0.2"))
+            .withExposedPorts(8080)
+            .waitingFor(Wait.forHttp("/auth/realms/master"));
 
     private JdbcTemplate jdbcTemplate;
 
     @Test
-    void containerForEachTestMethod() {
+    void postgresContainerTest() {
 
         Long result = jdbcTemplate.queryForObject("SELECT DISTINCT 1 FROM information_schema.tables", Long.class);
         posgtres.getLogs();
 
         assertEquals(1L, result);
+    }
 
+    @Test
+    void keycloakContainerTest() {
 
+        when()
+                .get("http://" + keycloak.getHost() + ":" + keycloak.getMappedPort(8080) + "/auth/realms/master/.well-known/openid-configuration")
+                .prettyPeek()
+        .then()
+                .statusCode(200);
     }
 
 
 
     @BeforeEach
-    void init() throws IllegalAccessException, InvocationTargetException, InstantiationException {
+    void init() {
         final String driverClassName = posgtres.getDriverClassName();
         final String jdbcUrl = posgtres.getJdbcUrl();
         final String username = posgtres.getUsername();
         final String password = posgtres.getPassword();
 
-
-        final Class<?> driverClass = ClassUtils.resolveClassName(driverClassName, this.getClass().getClassLoader());
-        final Driver driver = (Driver) ClassUtils.getConstructorIfAvailable(driverClass).newInstance();
-        final DataSource dataSource = DataSourceBuilder.create().driverClassName(driverClassName).url(jdbcUrl).username(username).password(password).build();
+        final DataSource dataSource = DataSourceBuilder.create()
+                .driverClassName(driverClassName)
+                .url(jdbcUrl)
+                .username(username)
+                .password(password).build();
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 }
